64c18fbca24db1cd9620897915bc7bb2b96b2546
pole <povilasleka@gmail.com>
2020-04-02 18:05:10 +0300

    Lang

--- src/components/LocaleSelector/index.js
+++ src/components/LocaleSelector/index.js
@@ -1,20 +1,21 @@
 import React, { useContext } from 'react'
 import StoreContext from '../../context/StoreContext'
-import { Select, Option } from './styles'
+import { Select, Option, Wrapper } from './styles'
 
 const LocaleSelector = () => {
     const { locale, changeLocale } = useContext(StoreContext);
 
     return (
-        <>
-            <Select class="form-control" onChange={e => changeLocale(e.target.value)} value={locale}>
+        <Wrapper>
+            <p>Language:</p>
+            <Select onChange={e => changeLocale(e.target.value)} value={locale} data-width="fit">
                 <Option value="en">English</Option>
                 <Option value="it">Italiano</Option>
                 <Option value="de">Deutsch</Option>
                 <Option value="es">Español</Option>
                 <Option value="fr">Le français</Option>
             </Select>
-        </>
+        </Wrapper>
     );
 }
 
--- src/components/LocaleSelector/styles.js
+++ src/components/LocaleSelector/styles.js
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled'
 
+export const Wrapper = styled.div`
+    p {
+        display: inline;
+        color: whitesmoke;
+        padding-right: 1rem;
+    }
+`;
+
 export const Select = styled.select`
-    width: 140px;
     background: none;
     border: none;
     outline: none;
     color: whitesmoke;
+    font-weight: bold;
 `;
 
 export const Option = styled.option`
