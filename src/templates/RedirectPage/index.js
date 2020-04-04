435b6836c604dcfee05b2f7ce2de4868327017cf
Povilas <povilasleka@gmail.com>
2020-04-04 20:11:18 +0300

    all

--- src/templates/RedirectPage/index.js
+++ src/templates/RedirectPage/index.js
@@ -4,8 +4,11 @@ import { graphql } from 'gatsby'
 const RedirectPage = ({ data }) => {
     return (
         <>
-            <h1>{data.site.siteMetadata.seotitle}</h1>
-            <p>{data.site.siteMetadata.description}</p>
+            Shield4face.com
+            <h1>Buy Face Protection Shield. Fast Shipping</h1>
+            <p>Professional shields for the face are engineered to be comfortable, convenient, and functional to keep your face protected.
+            Wear it if there is a risk to get infected by a disease or if you might pass it on others.
+    Faceshield K95 has CE certification.</p>
         </>
     );
 }
